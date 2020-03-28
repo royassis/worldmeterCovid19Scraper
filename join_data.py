9f9b02c6105e109699fff3b15f1a162438f90dce
royassis <royassisg@gmail.com>
2020-03-28 12:52:18 +0300

    Update refs files

--- join_data.py
+++ join_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import glob
+import re
 
 path = r'data'
 all_files = glob.glob(path + "/*.csv")
@@ -11,5 +12,16 @@ for filename in all_files:
     df = df.iloc[:,1:]
     li.append(df)
 
-[df.shape for df in li]
+li[-1].columns
+
 frame = pd.concat(li[:-10], axis=0, ignore_index=True,sort=True)
+
+
+conversion_dict={
+    '.*\sCases'         : 'NewCases',
+    '^Cases$'           : 'NewCases',
+    '^Change.*Cases'    : 'NewCases'
+}
+
+for pat,str in conversion_dict.items():
+    frame = frame.rename(columns=lambda x: re.sub(pat,str,x))
