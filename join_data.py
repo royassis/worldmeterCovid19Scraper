7eca9a6fe78a0af07ea4ca954aa82122aa544495
royassis <royassisg@gmail.com>
2020-03-28 13:07:27 +0300

    Update refs files

--- join_data.py
+++ join_data.py
@@ -5,35 +5,23 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 
-path = r'data'
-all_files = glob.glob(path + "/*.csv")
-
-li = []
-
-for filename in all_files:
-    df = pd.read_csv(filename, index_col=[0], header=0)
-    df = df.iloc[:,1:]
-    li.append(df)
-
-li[-1].columns
-
-frame = pd.concat(li[:-10], axis=0, ignore_index=True,sort=True)
-
-
 conversion_dict={
     '.*\sCases'             : 'New Cases',
     '^Cases$'               : 'New Cases',
     '^Change.*Cases.*'      : 'New Cases',
     '^New Today$'           : 'New Cases',
     '^Change Today$'        : 'New Cases',
+    '^new.*cases$'          : 'New Cases',
 
     'ActiveCases'           : 'Total Cases',
     'TotalCases'            : 'Total Cases',
 
     '.*\sDeaths'            : 'Total Deaths',
+    '^Deaths$'              : 'Total Deaths',
     '^TotalDeaths$'         : 'Total Deaths',
 
     '^Change.*Deaths.*'     : 'New Deaths',
+    '^new.*Deaths'          : 'New Deaths',
 
     'Serious.*,.*Critical'  : 'Serious_Critical',
 
@@ -41,7 +29,25 @@ conversion_dict={
 
 }
 
-for pat,str in conversion_dict.items():
-    frame = frame.rename(columns=lambda x: re.sub(pat,str,x, flags=re.IGNORECASE))
+
+path = r'data'
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+
+# Iterate and read csv files into df
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=[0], header=0)
+    df = df.iloc[:,1:]
+
+    # Rename columns
+    for pat, str in conversion_dict.items():
+        df = df.rename(columns=lambda x: re.sub(pat, str, x, flags=re.IGNORECASE))
+
+    # Append to df list
+    li.append(df)
+
+
+frame = pd.concat(li[:-10], axis=0, ignore_index=True,sort=True)
 
 pp.pprint(set(frame.columns))
