689d48094cd072d765d78f078d91ac14e36fe3e5
royassis <royassisg@gmail.com>
2020-03-28 13:02:47 +0300

    Update refs files

--- join_data.py
+++ join_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import glob
 import re
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 path = r'data'
 all_files = glob.glob(path + "/*.csv")
@@ -18,10 +21,27 @@ frame = pd.concat(li[:-10], axis=0, ignore_index=True,sort=True)
 
 
 conversion_dict={
-    '.*\sCases'         : 'NewCases',
-    '^Cases$'           : 'NewCases',
-    '^Change.*Cases'    : 'NewCases'
+    '.*\sCases'             : 'New Cases',
+    '^Cases$'               : 'New Cases',
+    '^Change.*Cases.*'      : 'New Cases',
+    '^New Today$'           : 'New Cases',
+    '^Change Today$'        : 'New Cases',
+
+    'ActiveCases'           : 'Total Cases',
+    'TotalCases'            : 'Total Cases',
+
+    '.*\sDeaths'            : 'Total Deaths',
+    '^TotalDeaths$'         : 'Total Deaths',
+
+    '^Change.*Deaths.*'     : 'New Deaths',
+
+    'Serious.*,.*Critical'  : 'Serious_Critical',
+
+    'TotalRecovered'        : 'Total Recovered'
+
 }
 
 for pat,str in conversion_dict.items():
-    frame = frame.rename(columns=lambda x: re.sub(pat,str,x))
+    frame = frame.rename(columns=lambda x: re.sub(pat,str,x, flags=re.IGNORECASE))
+
+pp.pprint(set(frame.columns))
