ee492fc81bf7ef478b678b3f2095b54f0ffe7c31
royassis <royassisg@gmail.com>
2020-03-29 18:00:37 +0300

    cmnt

--- settings.py
+++ settings.py
@@ -5,17 +5,20 @@ from selenium import webdriver
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
+# Paths and dirs
 resource_dir = 'resources'
 data_dir = 'data'
 log_dir = 'logs'
 
+hrefs_path = os.path.join(resource_dir, 'refs.csv')
+hrefs = pd.read_csv(hrefs_path, index_col ='id')
+
+# Selenium options
 chromedriver_path=r"C:\Users\roy\Desktop\chromedriver.exe"
 option = webdriver.ChromeOptions()
 option.add_argument('— incognito')
 
-hrefs_path = os.path.join(resource_dir, 'refs.csv')
-hrefs = pd.read_csv(hrefs_path, index_col ='id')
-
+# Resources
 mapper_path = os.path.join(resource_dir, 'column_remapper.csv')
 mapper = pd.read_csv(mapper_path, index_col = 'key', usecols = ['key','value'])
 column_remapper = mapper.iloc[:,0]
