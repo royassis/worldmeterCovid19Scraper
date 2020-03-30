49cc5d885b5d4dadc43ae5ca6f493904dc010c8a
royassis <royassisg@gmail.com>
2020-03-30 20:23:15 +0300

    cmnt

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ hrefs_path = os.path.join(resource_dir, 'refs.csv')
 hrefs = pd.read_csv(hrefs_path, index_col ='id')
 
 # Selenium options
-chromedriver_path=r"C:\Users\roy\Desktop\chromedriver.exe"
+chromedriver_path= os.path.join(resource_dir, 'chromedriver.exe')
 option = webdriver.ChromeOptions()
 option.add_argument('— incognito')
 
